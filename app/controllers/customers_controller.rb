5cdd0b7f3500d88e587f92d9469472f72d52b078
sho-itsuka <sho.itsuka122@gmail.com>
2022-04-20 17:54:27 +0900

    顧客新規登録

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -8,10 +8,15 @@ class CustomersController < ApplicationController
 
   def create
     @customer = Customer.new(customer_params)
+    if @customer.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
   def customer_params
-    params.require(:customer).permit(:nickname, :salon_name)
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :gender_id, :phone_number, :email, :birthday, :postal_code, :prefecture_id, :city, :address, :building)
   end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,13 +8,10 @@ class Customer < ApplicationRecord
   validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: "is invalid. Enter it as follows(e.g. 123-4567)" }
   
   with_options presence: true, format: { with: /\A0[5789]0-?\d{4}-?\d{4}\z/, message: 'is invalid. Input only number' } do
-    validates :phonenumber
+    validates :phone_number
   end
-  validates :phonenumber, format: { with: /\A\d{10,11}\z/, message: 'is too short' }
 
-  with_options presence: true, format: { with: /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/} do
-    validates :email
-  end
+  validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is too short' }
   
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
     validates :first_name
