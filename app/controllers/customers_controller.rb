75b73ff0ee971a17ea377b9ef215f7de84e37124
sho-itsuka <sho.itsuka122@gmail.com>
2022-04-21 16:04:09 +0900

    顧客情報編集・更新

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,6 @@
 class CustomersController < ApplicationController
+  before_action :set_customer, only: %i[edit show update]
+
   def index
     @customers = Customer.all
   end
@@ -17,11 +19,26 @@ class CustomersController < ApplicationController
   end
 
   def show
-    @customer = Customer.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @customer.update(customer_params)
+      redirect_to customer_path
+    else
+      render :edit
+    end
   end
 
   private
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :gender_id, :phone_number, :email, :birthday, :postal_code, :prefecture_id, :city, :address, :building).merge(user_id: current_user.id)
   end
+
+  def set_customer
+    @customer = Customer.find(params[:id])
+  end
+  
 end
