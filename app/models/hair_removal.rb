8f7bee36307ba7dc272adf79a76c76dfba8d11f9
sho-itsuka <sho.itsuka122@gmail.com>
2022-04-24 15:09:11 +0900

    ActiveStorageの導入

--- app/models/hair_removal.rb
+++ app/models/hair_removal.rb
@@ -2,6 +2,7 @@ class HairRemoval < ApplicationRecord
   belongs_to :customer
   belongs_to :treatment
   belongs_to :part
+  has_one_attached :image
 
   validates :treatment_id, :part_id, numericality: { other_than: 1 } 
 
